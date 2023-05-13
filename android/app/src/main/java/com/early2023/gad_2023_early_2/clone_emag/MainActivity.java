34150956323fa7ef604af2af4526793fe255349d
AntonioGherman <antoniogherman15@gmail.com>
2023-05-13 19:44:25 +0300

    import project on github

---
+++ android/app/src/main/java/com/early2023/gad_2023_early_2/clone_emag/MainActivity.java
@@ -0,0 +1,6 @@
+package com.early2023.gad_2023_early_2.clone_emag;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/src/models/index.dart
@@ -0,0 +1,7 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'index.freezed.dart';
+part 'index.g.dart';
+part 'auth/app_user.dart';
+part 'auth/auth_state.dart';
+part 'app_state.dart';
